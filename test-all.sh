b3293aa543646e90357c604a1b9f57a1af92afbc
andy-faunadb <andy@faunadb.com>
2015-10-06 11:11:28 -0700

    Don't use hardcoded latest version in test-all.sh

--- test-all.sh
+++ test-all.sh
@@ -2,4 +2,4 @@ source ~/.nvm/nvm.sh
 nvm install 0.10.0; npm run test
 nvm install 0.12.7; npm run test
 nvm install 4.0.0; npm run test
-nvm install 4.1.1; npm run test
+nvm install stable; npm run test
