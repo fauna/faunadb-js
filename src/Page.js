57ef2d078327ed6cce6fdeffc5b46b2380fd94a7
Ed Ceaser <ed@faunadb.com>
2016-06-03 17:33:31 -0700

    wip

---
+++ src/Page.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var query = require('./query');
+
+/**
+ *
+ * @param {Client} client
+ * @param set
+ * @constructor
+ */
+function Page(client, set) {
+  this.client = client;
+  this.set = set;
+  /**
+   * @member {Array.<Function>}
+   * @type {Array.<Function>}
+   * @private
+   */
+  this._fauna_functions = [];
+}
+
+Page.prototype.map = function(lambda) {
+  var rv = this.clone();
+  rv._fauna_functions.push(function(q) { query.map(q, lambda) });
+  return rv;
+};
+
+Page.prototype.filter = function(lambda) {
+ 
+};
+
+Page.prototype.iterator = function() {
+  
+};
+
+Page.prototype.each = function(lambda) {
+  return this._next_page().then(this._handle_page(lambda));
+};
+
+Page.prototype._handle_page = function(lambda) {
+  var self = this;
+  return function (page) {
+    lambda(page.data);
+
+    if (page.after !== undefined) {
+      return self._next_page(page.after).then(self._handle_page(lambda));
+    } else {
+      return Promise.resolve();
+    }
+  };
+};
+
+/**
+ *
+ * @returns {Promise.<Object>}
+ * @private
+ */
+Page.prototype._next_page = function(cursor) {
+  var opts = {};
+  if (cursor !== undefined) {
+    opts.after = cursor;
+  }
+
+  // TODO: ADD PARAMS
+  var q = query.paginate(this.set, opts);
+
+  if (this._fauna_functions.length > 0) {
+    this._fauna_functions.forEach(function(lambda) {
+      q = lambda(q);
+    });
+  }
+
+  return this.client.query(q);
+};
+
+Page.prototype.clone = function() {
+  var rv = {
+    client: this.client,
+    set: this.set,
+    _fauna_functions: this._fauna_functions
+  };
+
+  rv.prototype = Page;
+  return rv;
+};
+
+module.exports = Page;
