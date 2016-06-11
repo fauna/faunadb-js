b44de1f09a61722d56d9822ddb0f6e7dbe1f25d0
Ed Ceaser <ed@faunadb.com>
2016-06-10 17:40:44 -0700

    wip

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  Client: require('./src/Client'),
+  clientLogger: require('./src/clientLogger'),
+  errors: require('./src/errors'),
+  Expr: require('./src/Expr'),
+  objects: require('./src/objects'),
+  PageHelper: require('./src/PageHelper'),
+  query: require('./src/query'),
+  RequestResult: require('./src/RequestResult')
+};
