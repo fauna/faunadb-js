2d5fded023b2bbfe0ed81ea4ed5eb271c85d0de3
Erick Pintor <erickpintor@gmail.com>
2017-07-03 16:22:48 -0300

    fix typescript declaration file to include default types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
+export { default as Client } from './src/types/Client';
+export { default as Expr } from './src/types/Expr';
+export { default as PageHelper } from './src/types/PageHelper';
+export { default as RequestResult } from './src/types/RequestResult';
+
 export * from './src/types/Client';
 export * from './src/types/errors';
 export * from './src/types/Expr';
