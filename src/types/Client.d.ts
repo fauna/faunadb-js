863bd283cf3e9797b32c798ac101994a3e1426c8
Ed Ceaser <ed@fauna.com>
2017-11-22 19:10:10 -0800

    remove nonexistent client functions

--- src/types/Client.d.ts
+++ src/types/Client.d.ts
@@ -16,12 +16,5 @@ export default class Client {
 
   query(expr: Expr): Promise<object>;
   paginate(expr: Expr): PageHelper;
-
-  get(path: string, query?: object): Promise<object>;
-  post(path: string, data: object): Promise<object>;
-  put(path: string, data: object): Promise<object>;
-  patch(path: string, data: object): Promise<object>;
-  delete(path: string): Promise<object>;
-
   ping(scope?: string, timeout?: number): Promise<string>;
 }
