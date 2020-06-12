24d53eafac145ef86e6ddbd3b728ddec85cf0094
Ryan Harris <ryan.harris@fauna.com>
2020-06-12 13:44:00 -0400

    feat: add ContainsValue to types

--- src/types/query.d.ts
+++ src/types/query.d.ts
@@ -255,4 +255,5 @@ export module query {
 
   export function MoveDatabase(from: ExprArg, to: ExprArg): Expr
   export function Documents(collection: ExprArg): Expr
+  export function ContainsValue(value: ExprArg, _in: ExprArg): Expr
 }
