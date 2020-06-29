60ab0f4ce25bc39c87bde29e47ac994ae9a127cb
Gabin Desserprit <desserprit.gabin@gmail.com>
2020-06-29 16:49:31 +0300

    Fix type of Client.query expression parameter from Expr to ExprArg (#285)

--- src/types/Client.d.ts
+++ src/types/Client.d.ts
@@ -1,4 +1,5 @@
 import Expr from './Expr'
+import { ExprArg } from './query'
 import PageHelper from './PageHelper'
 import RequestResult from './RequestResult'
 
@@ -20,7 +21,7 @@ export interface QueryOptions {
 
 export default class Client {
   constructor(opts?: ClientConfig)
-  query<T = object>(expr: Expr, options?: QueryOptions): Promise<T>
+  query<T = object>(expr: ExprArg, options?: QueryOptions): Promise<T>
   paginate(expr: Expr, params?: object, options?: QueryOptions): PageHelper
   ping(scope?: string, timeout?: number): Promise<string>
 }
