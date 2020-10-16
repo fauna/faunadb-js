7c32cafc33a43023bc71eb35f17d729668168aa6
Ryan Harris <ryan.harris@fauna.com>
2020-10-16 10:44:54 -0400

    chore: update Subscription types

--- src/types/Stream.d.ts
+++ src/types/Stream.d.ts
@@ -2,28 +2,9 @@ import Client from './Client'
 import { ExprArg } from './query'
 
 export interface Subscription {
-  client: StreamClient
-  dispatcher: EventDispatcher
-}
-
-export interface EventDispatcher {
-  allowedEvents: string[]
-}
-
-export interface StreamClient {
-  client: Client
-  expression: ExprArg
-  options: Options
-  onEvent: Function
-}
-
-export interface Options {
-  ref: string
-  ts: string
-  new: string
-  old: string
-  diff: string
-  action: string
+  on: Function
+  start: Function
+  close: Function
 }
 
 export interface ClientStreamOptions {
