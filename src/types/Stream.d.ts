5d8057fabe46ff660def0f23accf924ad6d501e2
Ryan Harris <ryan.harris@fauna.com>
2020-10-19 18:07:02 -0400

    chore: update TS types

--- src/types/Stream.d.ts
+++ src/types/Stream.d.ts
@@ -1,10 +1,40 @@
 import Client from './Client'
 import { ExprArg } from './query'
+import { FaunaTime, Ref } from './values'
 
 export interface Subscription {
   on: (type: SubscriptionEvents, callback: Function) => this
   start: () => this
   close: () => this
+  event: string
+  txnTS: number
+  data:
+    | StreamStartEventData
+    | StreamVersionEventData
+    | StreamErrorEventData
+    | StreamHistoryRewriteEventData
 }
 
-type SubscriptionEvents = 'ref' | 'ts' | 'new' | 'old' | 'diff' | 'action'
+type StreamStartEventData = FaunaTime
+
+type StreamVersionEventData = {
+  ref: Ref
+  ts: number
+  action: string
+  new: object
+  old: object
+  diff: string
+}
+
+type StreamErrorEventData = {
+  code: number
+  description: string
+}
+
+type StreamHistoryRewriteEventData = {
+  ref: Ref
+  ts: number
+  action: string
+}
+
+type SubscriptionEvents = 'start' | 'version' | 'history_rewrite' | 'error'
