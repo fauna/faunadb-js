dd6ab12a6318baaa5628b0418115be26eb95c46d
SZinkevych <s.zinkevych@gmail.com>
2021-02-05 10:21:17 +0200

    query imports

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ export { default as Client } from './src/types/Client';
 export { default as Expr } from './src/types/Expr';
 export { default as PageHelper } from './src/types/PageHelper';
 export { default as RequestResult } from './src/types/RequestResult';
+export * as query from './src/types/query';
 
 export * from './src/types/Client';
 export * from './src/types/errors';
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-module.exports = {
-  Client: require('./src/Client'),
-  Expr: require('./src/Expr'),
-  PageHelper: require('./src/PageHelper'),
-  RequestResult: require('./src/RequestResult'),
+var query = require('./src/query')
+var util = require('./src/_util')
 
-  clientLogger: require('./src/clientLogger'),
-  errors: require('./src/errors'),
-  values: require('./src/values'),
-  query: require('./src/query'),
-}
+module.exports = util.mergeObjects(
+  {
+    Client: require('./src/Client'),
+    Expr: require('./src/Expr'),
+    PageHelper: require('./src/PageHelper'),
+    RequestResult: require('./src/RequestResult'),
+
+    clientLogger: require('./src/clientLogger'),
+    errors: require('./src/errors'),
+    values: require('./src/values'),
+    query: query,
+  },
+  query
+)
