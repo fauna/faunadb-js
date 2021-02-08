d3afbdb4815e12d0e0797543ddb7d0c4239f657f
Sergii Zinkevych <s.zinkevych@gmail.com>
2021-02-08 09:55:22 +0200

    query imports (#403)

--- src/_util.js
+++ src/_util.js
@@ -194,7 +194,25 @@ function encode(input) {
   }
 }
 
+/**
+ * Merge two objects into one
+ * @param obj1
+ * @param obj2
+ * @returns obj3 a new object based on obj1 and obj2
+ */
+function mergeObjects(obj1, obj2) {
+  var obj3 = {}
+  for (var attrname in obj1) {
+    obj3[attrname] = obj1[attrname]
+  }
+  for (var attrname in obj2) {
+    obj3[attrname] = obj2[attrname]
+  }
+  return obj3
+}
+
 module.exports = {
+  mergeObjects: mergeObjects,
   formatUrl: formatUrl,
   inherits: inherits,
   isNodeEnv: isNodeEnv,
