556d47b307d4a3f426981ef524d0fb9abc452d98
SZinkevych <s.zinkevych@gmail.com>
2021-03-16 13:47:48 +0200

    tool

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
 const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+const packageJson = require('./package.json')
 
-module.exports = {
-  entry: path.resolve(__dirname, 'index.js'),
+module.exports = env => ({
+  entry: path.resolve(__dirname, 'src/index.js'),
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'wp_dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'faunadb.js',
     library: 'faunadb',
     libraryTarget: 'umd',
     globalObject: 'this',
   },
-  mode: 'production',
-}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  modules: 'umd',
+                  targets: packageJson.browserslist,
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  ...(env.analyze && { plugins: [new BundleAnalyzerPlugin()] }),
+})
