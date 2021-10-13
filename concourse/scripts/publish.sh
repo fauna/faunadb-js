72e636c68ebe3409052764f6eb265a376eb1ca6b
SZinkevych <s.zinkevych@gmail.com>
2021-10-13 11:14:24 +0200

    up

--- concourse/scripts/publish.sh
+++ concourse/scripts/publish.sh
@@ -18,11 +18,11 @@ if [ "$PACKAGE_VERSION" \> "$NPM_LATEST_VERSION" ]
 then
   echo "Publishing a new version..."
   echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
-  npm publish
-  rm .npmrc
+  # npm publish
+  # rm .npmrc
   echo "faunadb-js@$PACKAGE_VERSION publushed to npm" > slack-message
 else
-echo "NPM package already published on npm with version ${NPM_LATEST_VERSION}. Update version, please" > slack-message
+  echo "NPM package already published on npm with version ${NPM_LATEST_VERSION}. Update version, please" > slack-message
   echo "NPM package already published on npm with version ${NPM_LATEST_VERSION}" 1>&2
   exit 1
 fi
