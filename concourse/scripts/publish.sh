f1fae16d4e5ed820d997593b21c0565edb2bdda2
Sergii Zinkevych <s.zinkevych@gmail.com>
2021-10-13 15:06:22 +0200

    slack notification (#547)

--- concourse/scripts/publish.sh
+++ concourse/scripts/publish.sh
@@ -4,11 +4,6 @@ set -eou
 
 cd ./fauna-js-repository
 
-mkdir dist
-npm install
-npm run browserify
-npm run browserify-min
-
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 NPM_LATEST_VERSION=$(npm view faunadb version)
 echo "Current package version: $PACKAGE_VERSION"
@@ -16,11 +11,19 @@ echo "Latest version in npm: $NPM_LATEST_VERSION"
 
 if [ "$PACKAGE_VERSION" \> "$NPM_LATEST_VERSION" ]
 then
+  mkdir dist
+  npm install
+  npm run browserify
+  npm run browserify-min
+
   echo "Publishing a new version..."
   echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
   npm publish
   rm .npmrc
+
+  echo "faunadb-js@$PACKAGE_VERSION published to npm" > ../slack-message/publish
 else
-  echo "NPM package already published on npm with version ${NPM_LATEST_VERSION}" 1>&2
+  echo "faunadb-js@${NPM_LATEST_VERSION} package has been already published" > ../slack-message/publish
+  echo "faunadb-js@${NPM_LATEST_VERSION} package has been already published" 1>&2
   exit 1
 fi
