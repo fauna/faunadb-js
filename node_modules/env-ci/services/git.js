4dab35c9e404b998595faf30865d9123fd469518
Fauna, Inc <nobody@concourse-ci.org>
2022-01-28 20:14:35 +0000

    Update docs to version: 4.4.2

--- node_modules/env-ci/services/git.js
+++
@@ -1,7 +0,0 @@
-const {head, branch} = require('../lib/git.js');
-
-module.exports = {
-  configuration(options) {
-    return {commit: head(options), branch: branch(options)};
-  },
-};
--- node_modules/modify-source-webpack-plugin/build/loader.d.ts
+++
@@ -1,2 +0,0 @@
-declare function _exports(source: any): any;
-export = _exports;
