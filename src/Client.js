cc268a980c7e0f64f394d73a1f95d9e9f4096e26
Grey Elerson <grey.elerson@fauna.com>
2022-03-28 17:40:18 -0600

    Update src/Client.js
    Co-authored-by: Cleve Stuart <90649124+cleve-fauna@users.noreply.github.com>

--- src/Client.js
+++ src/Client.js
@@ -362,8 +362,9 @@ Client.prototype._execute = function(method, path, data, query, options) {
         return {
           value: responseObject['resource'],
           metrics: Object.fromEntries(Array.from(Object.entries(response.headers)).
-            map(([ k,v ]) => [k, parseInt(v)]).
-            filter( ([k,v]) => metricsHeaders.includes(k) )),
+            filter( ([k,v]) => metricsHeaders.includes(k) )).
+            map(([ k,v ]) => [k, parseInt(v)])
+            ,
         }
       } else {
         return responseObject['resource']
